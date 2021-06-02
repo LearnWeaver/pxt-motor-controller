abc0572df964ffb6ae1cfd1b9b98317b0a712df4
Daniel.Pollard <daniel.pollard@learnweaver.com>
2021-06-02 22:01:37 +1000

    reworked

--- motorcontroller.ts
+++ motorcontroller.ts
@@ -5,10 +5,15 @@
 namespace BBS_Motor_Controller {
 
     //Some useful parameters. 
-        let ChipAddress = 0x6A //default Chip address
+        let ChipAddress = 0x40 //default Chip address
         let PrescaleReg = 0xFE //the prescale register address
         let Mode1Reg = 0x00  //The mode 1 register address
-        
+
+        let SERVOS = 0x06; // first servo address for start byte low
+        let servoTarget: number[] = [];
+        let servoActual: number[] = [];
+        let servoCancel: boolean[] = [];
+            
     // If you wanted to write some code that stepped through the servos then this is the BASe and size to do that 	
         let Servo1RegBase = 0x08 
         let ServoRegDistance = 4
@@ -102,6 +107,176 @@ namespace BBS_Motor_Controller {
     
             }
         }
+
+        /**
+         * Initialise all servos to Angle=0
+         */
+        //% blockId="centreServos"
+        //% block="centre all servos"
+        export function centreServos(): void
+        {
+           
+            setServo(Servos.Servo1, 0);
+            etServo(Servos.Servo2, 0);
+        }
+
+        /**
+         * Set Servo Position by Angle
+         * @param servo Servo number (0 to 15)
+         * @param angle degrees to turn servo (-90 to +90)
+         */
+        //% blockId="an_setServo" block="set servo %servo| to angle %angle"
+        //% weight=70
+        //% angle.min=-90 angle.max.max=90
+        export function setServo(servo: Servos, angle: number): void
+        {
+            let servoNumber = 15;
+            if(servo == Servos.Servo2){
+                servoNumber = 16;
+            }
+            setServoRaw(servoNumber, angle);
+            servoTarget[servoNumber] = angle;
+        }
+
+
+        function setServoRaw(servo: Servos, angle: number): void
+        {
+            if (initalised == false)
+            {
+                secretIncantation();
+            }
+            // two bytes need setting for start and stop positions of the servo
+            // servos start at SERVOS (0x06) and are then consecutive blocks of 4 bytes
+            // the start position (always 0x00) is set during init for all servos
+
+            let i2cData = pins.createBuffer(2);
+            let start = 0;
+            angle = Math.max(Math.min(90, angle),-90);
+            let stop = 369 + angle * 223 / 90;
+
+            i2cData[0] = SERVOS + servo*4 + 2;	// Servo register
+            i2cData[1] = (stop & 0xff);		// low byte stop
+            pins.i2cWriteBuffer(ChipAddress, i2cData, false);
+
+            i2cData[0] = SERVOS + servo*4 + 3;	// Servo register
+            i2cData[1] = (stop >> 8);		// high byte stop
+            pins.i2cWriteBuffer(ChipAddress, i2cData, false);
+            servoActual[servo] = angle;
+        }
+
+
+            /**
+         * Move Servo to Target Position at selected Speed
+         * @param servo Servo number (0 to 15)
+         * @param angle degrees to turn to (-90 to +90)
+         * @param speed degrees per second to move (1 to 1000) eg: 60
+         */
+        //% blockId="moveServo" block="move servo %servo| to angle %angle| at speed %speed| degrees/sec"
+        //% weight=70
+        //% angle.min=-90 angle.max.max=90
+        //% speed.min=1 speed.max=1000
+        //% subcategory=Servos
+        export function moveServo(servo: Servos, angle: number, speed: number): void
+        {
+            let servoNumber = 15;
+            if(servo == Servos.Servo2){
+                servoNumber = 16;
+            }
+            let step = 1;
+            let delay = 10; // 10ms delay between steps
+            if(servoTarget[servoNumber] != servoActual[servoNumber])   // cancel any existing movement on this servo?
+            {
+                servoCancel[servoNumber] = true;
+                while(servoCancel[servoNumber])
+                    basic.pause(1);  // yield
+            }
+            angle = Math.max(Math.min(90, angle),-90);
+            speed = Math.max(Math.min(1000, speed),1);
+            delay = Math.round(1000/speed);
+            servoTarget[servoNumber] = angle;
+            if (angle < servoActual[servoNumber])
+                step = -1;
+            control.inBackground(() =>
+            {
+                while (servoActual[servoNumber] != servoTarget[servoNumber])
+                {
+                    if(servoCancel[servoNumber])
+                    {
+                        servoCancel[servoNumber] = false;
+                        break;
+                    }
+                    setServoRaw(servoNumber, servoActual[servoNumber]+step);
+                    basic.pause(delay);
+                }
+            })
+        }
+
+        /**
+         * Get Servo Current Actual Position
+         * @param servo Servo number (0 to 15)
+         */
+        //% blockId="getServoActual" block="servo %servo| actual position"
+        //% weight=10
+    
+        export function getServoActual(servo: Servos): number
+        {
+            let servoNumber = 15;
+            if(servo == Servos.Servo2){
+                servoNumber = 16;
+            }
+
+            return servoActual[servoNumber];
+        }
+
+        /**
+         * Get Servo Target Position
+         * @param servo Servo number (0 to 15)
+         */
+        //% blockId="getServoTarget" block="servo %servo| target position"
+        //% weight=8
+        
+        export function getServoTarget(servo: Servos): number
+        {
+            let servoNumber = 15;
+            if(servo == Servos.Servo2){
+                servoNumber = 16;
+            }
+            return servoTarget[servoNumber];
+        }
+
+        /**
+         * Check if servo has reached target
+         * @param servo Servo number (0 to 15)
+         */
+        //% blockId="isServoDone" block="servo %servo| is complete"
+        //% weight=5
+        //% subcategory=Servos
+        export function isServoDone(servo: Servos): boolean
+        {
+            let servoNumber = 15;
+            if(servo == Servos.Servo2){
+                servoNumber = 16;
+            }
+            return servoTarget[servoNumber]==servoActual[servoNumber];
+        }
+
+        /**
+         * Wait until servo has reached target position
+         * @param servo Servo number (0 to 15)
+         */
+        //% blockId="waitServo" block="wait for servo %servo"
+        //% weight=5
+        //% subcategory=Servos
+        export function waitServo(servo: Servos): void
+        {
+            let servoNumber = 15;
+            if(servo == Servos.Servo2){
+                servoNumber = 16;
+            }
+
+            while (servoActual[servoNumber] != servoTarget[servoNumber]) // what if nothing is changing these values?
+                basic.pause(10);
+        }
     
         /*
             This secret incantation sets up the PCA9865 I2C driver chip to be running at 50Hx pulse repetition, and then sets the 16 output registers to 1.5mS - centre travel.
@@ -112,27 +287,35 @@ namespace BBS_Motor_Controller {
             let buf = pins.createBuffer(2)
     
            
-            // First set the prescaler to 50 hz
-            buf[0] = PrescaleReg
-            buf[1] = 0x85
+            
+            buf[0] = 0  // Mode 1 register
+            buf[1] = 0x10 // put to sleep
             pins.i2cWriteBuffer(ChipAddress, buf, false)
-            //Block write via the all leds register to set all of them to 90 degrees
-            buf[0] = 0xFA
-            buf[1] = 0x00
+           
+            buf[0] = 0xFE;	// Prescale register
+            buf[1] = 101;	// set to 60 Hz
             pins.i2cWriteBuffer(ChipAddress, buf, false)
-            buf[0] = 0xFB
-            buf[1] = 0x00
+            buf[0] = 0;		// Mode 1 register
+            buf[1] = 0x81;	// Wake up
             pins.i2cWriteBuffer(ChipAddress, buf, false)
-            buf[0] = 0xFC
-            buf[1] = 0x66
-            pins.i2cWriteBuffer(ChipAddress, buf, false)
-            buf[0] = 0xFD
-            buf[1] = 0x00
-            pins.i2cWriteBuffer(ChipAddress, buf, false)
-            //Set the mode 1 register to come out of sleep
-            buf[0] = Mode1Reg
-            buf[1] = 0x01
-            pins.i2cWriteBuffer(ChipAddress, buf, false)
+            
+
+            for (let servo=0; servo<16; servo++)
+            {
+                buf[0] = SERVOS + servo*4 + 0;	// Servo register
+                buf[1] = 0x00;			// low byte start - always 0
+                pins.i2cWriteBuffer(ChipAddress, buf, false);
+
+                buf[0] = SERVOS + servo*4 + 1;	// Servo register
+                buf[1] = 0x00;			// high byte start - always 0
+                pins.i2cWriteBuffer(ChipAddress, buf, false);
+
+                servoTarget[servo]=0;
+                servoActual[servo]=0;
+                servoCancel[servo]=false;
+
+            
+            }
             //set the initalised flag so we dont come in here again automatically
             initalised = true
         }
@@ -268,44 +451,6 @@ namespace BBS_Motor_Controller {
         }
         
         
-    /**
-         * sets the requested servo to the reguested angle.
-         * if the PCA has not yet been initialised calls the initialisation routine
-         *
-         * @param Servo Which servo to set
-         * @param degrees the angle to set the servo to
-         */
-        //% blockId=bbs_I2Cservo_write
-        //% block="set%Servo|to%degrees"
-        //% degrees.min=0 degrees.max=180
-        
-        export function servoWrite(Servo: Servos, degrees: number): void {
-            if (initalised == false) {
-                secretIncantation()
-            }
-            let buf = pins.createBuffer(2)
-            let HighByte = false
-            let deg100 = degrees * 100
-            let PWMVal100 = deg100 * ServoMultiplier
-            let PWMVal = PWMVal100 / 10000
-            PWMVal = Math.floor(PWMVal)
-            PWMVal = PWMVal + ServoZeroOffset
-            if (PWMVal > 0xFF) {
-                HighByte = true
-            }
-            buf[0] = Servo
-            buf[1] = PWMVal
-            pins.i2cWriteBuffer(ChipAddress, buf, false)
-            if (HighByte) {
-                buf[0] = Servo + 1
-                buf[1] = 0x01
-            }
-            else {
-                buf[0] = Servo + 1
-                buf[1] = 0x00
-            }
-            pins.i2cWriteBuffer(ChipAddress, buf, false)
-        }
             
     
     }
